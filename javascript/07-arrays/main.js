48e08f85ede00c1f8baea238480090b33c5286ab
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-04 09:37:26 +0000

    bucles

--- javascript/07-arrays/main.js
+++ javascript/07-arrays/main.js
@@ -19,5 +19,63 @@ myNames[6] = 'Amanda'
 console.log(myNames);
 
 
+//* Añadir algo al final con array.length. es como el push
+
+myNames[myNames.length] = 'Bryan'
+
+console.log(myNames);
+
+//*===================================================*//
+
+//* los strings funcionan 'parecido' a los arrays
+
+const palabra = 'alegria'
+
+console.log(palabra[3]);
+
+
+//* ==================================
+
+let valor1 = 23; 
+let valor2 = valor1; 
+
+console.log(valor1, valor2);
+
+
+valor1 = 'hola'
+
+console.log(valor1, valor2);
+
+
+const arr1 = [1, 2]
+const arr2 = arr1
+
+console.log(arr1, arr2);
+
+
+arr1[0] = 'hola'
+
+console.log(arr1, arr2);
+
+//* Como duplico un array sin modificar el original: ---> spread operator
+
+const arr3 = [...arr1]
+console.log(arr3);
+
+arr1[1] = 'osian'
+
+console.log(arr3);
+
+//! arr3 es una copia nueva, no como los dos anteriores. 
+
+console.log(...arr1, '👩‍🦲', ...arr3);
+
+let arr4 = [5, 6, [4, 5]]
+
+
+//* con este metodo hacemos un clon de los arrys normal y profundo. todo duplicado con referencias nuevas
+numeros5 = structuredClone(arr4)
+console.log(numeros5);
+//* esta es la unica manera de copiar los arrays profundos
 
 
---
+++ javascript/08-bucles/main.js
@@ -0,0 +1,13 @@
+//? Bucles en js vanilla
+
+//* estructuras de datos para: 
+
+//* 1. repetir codigo un numero de veces
+
+//* 2. recorrer elementos ITERABLES. (arrays, objetos, string...)
+
+//* for...
+
+for (let i = 1; i < 5; i += 2) {
+console.log('hola')
+}
