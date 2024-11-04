d55f71bc517baae6fad74d6f254f3eaf32054b13
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-04 08:46:49 +0000

    arrays

--- javascript/06-metodos-string/main.js
+++ javascript/06-metodos-string/main.js
@@ -17,4 +17,31 @@ console.log(frase.toLowerCase());
 const word = '  nota   ';
 console.log(word.trim())
 
+//* .indexOf('substring') --> te devuelve el indice del trozo de texte que le has pasado, no lo encuentra devuelve -1
 
+console.log(frase.indexOf('canoas'));
+console.log(frase.indexOf('perra'));
+
+
+console.log(frase.lastIndexOf('n'));
+
+
+//* .includes('substring') --> true o falso si se encuentra o no  el trozo de texto que le has pasado
+
+console.log(frase.includes('con'))
+console.log(frase.includes('band'))
+
+//* .slice(inicio corte, final corte) --> te devuelve un pedazo del texto, sin modificar el original
+
+trozo = frase.slice(4)
+console.log(trozo);
+
+trozo = frase.slice(4, 10)
+console.log(trozo);
+
+trozo = frase.slice(4, -5) //* negativo cuenta desde atras
+console.log(trozo);
+
+//* .split('separador') --> genera un nuevo array a partir del texto dado y separa el texto segurn el separador que le hemos pasado
+
+console.log(frase.split(' '))
---
+++ javascript/07-arrays/main.js
@@ -0,0 +1,23 @@
+//? Arrays
+
+const myNames = ['jesi', 'osi', 'peter']
+
+console.log(myNames);
+console.log(myNames['1']);
+console.log(myNames['2']);
+console.log(myNames['0']);
+
+console.log(myNames.length)
+
+//! puedo cambiar los elementos aunque la varible se const
+myNames[1] =  'juan'
+
+console.log(myNames);
+
+myNames[6] = 'Amanda'
+
+console.log(myNames);
+
+
+
+
