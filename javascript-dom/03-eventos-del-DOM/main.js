bf4db50cc6090ca4101d0a90853a64792a47053d
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-08 14:24:06 +0000

    arrow function in eventListener

--- javascript-dom/03-eventos-del-DOM/main.js
+++ javascript-dom/03-eventos-del-DOM/main.js
@@ -17,5 +17,47 @@ padding: 1rem 2rem;
 document.body.append(newBtn)
 
 
+//* EVENTOS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//cazamos el elemento (objeto del DOM)
+
 const img = document.querySelector('.foto-1')
 console.log(img);
+const h1 = document.querySelector('h1')
+console.log(h1);
+
+//escuchamos el evento que queramos
+function handleImageClick () {
+    console.log('has hedcho click en la imagen para cambiar el color de h1')
+   
+    h1.classList.add('colorOnClick')
+}
+
+
+//* version 1. creamos la funcion fuera y se la pasamos al eventListener
+
+img.addEventListener('click', handleImageClick )
+
+
+
+//* version 2. creamos una funcion anonima directamente dentro como parámetro
+
+img.addEventListener('click', function () {h1.classList.add('textSizeOnClick')} )
+
+
+//* version 3. creamos una funcion anonima directamente dentro como parámetro con arrow function
+
+newBtn.addEventListener('click', () => h1.classList.add('borderOnClick'))
+
+//*version 4. pasarle parámetros a la funcion que le estamos pasando al eventListener
+
+function handleImageClickWithParam (nombre) {
+    console.log(`${nombre} has hecho click en la imagen para que el h1 diga tu nombre`); 
+   
+    h1.innerHTML = nombre; 
+}
+
+img.addEventListener('click', function () {handleImageClickWithParam('osian')} )
+
+img.addEventListener('click', () => {handleImageClickWithParam('osian')} )
+
+img.addEventListener('click', () => handleImageClickWithParam('osian') )
